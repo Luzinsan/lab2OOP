85de61d0d5216bd139e23f3c2344b30e615fc35c
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2021-09-11 22:59:39 +0700

    Update lab2LuzinsanOOP.cpp

--- lab2LuzinsanOOP.cpp
+++ lab2LuzinsanOOP.cpp
@@ -1,12 +1,24 @@
-﻿#include <iostream>
 #include <ctime>
 #include "Matrix.h"
 
+using namespace luzalex;
+
 int main()
 {
+    setlocale(LC_ALL, "Russian");
     srand(time(0));
     rand();
 
+    std::cout << "asterix: \n" << std::endl;
+    Matrix asterix;
+    std::cout << "masterix: \n" << std::endl;
+    Matrix masterix(rand()%10);
+    std::cout << "matrix: \n" << std::endl;
     Matrix matrix(rand()%10, rand() % 10);
-    
+    std::cout << "maMatrix: \n" << std::endl;
+    Matrix maMatrix(matrix);
+    std::cout << "abelix: \n" << std::endl;
+    Matrix abelix(matrix + maMatrix);
+
+
 }
