069b40d30aedb519ba53e06d99365111f9b814e3
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2021-09-11 23:01:34 +0700

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,19 +1,41 @@
 #include "Matrix.h"
-#include <ctime>
-#include <iostream>
 
-Matrix::Matrix(int m, int n) : m_m{ (m % 10 + 1) }, m_n{ (n % 10 + 1) }
+namespace luzalex
 {
-    array = new double* [m_m];
-    std::cout << m_m << " " << m_n << std::endl;
-    for (int i = 0; i < m_m; ++i)
+    Matrix::Matrix(int m, int n) : m_m{ (m % 11) }, m_n{ (n % 11) }
     {
-        array[i] = new double[m_n];
-        for (int j = 0; j < m_n; ++j)
-        { 
-            array[i][j] = rand() % 100; 
-            printf("%3.0f ", array[i][j]);
+        std::cout << "Конструктор с параметрами m = " << m << " и n = " << n << std::endl;
+        array = new double* [m_m];
+        std::cout << m_m << " " << m_n << std::endl;
+        for (int i = 0; i < m_m; ++i)
+        {
+            array[i] = new double[m_n];
+            for (int j = 0; j < m_n; ++j)
+            {
+                array[i][j] = rand() % 100;
+                printf("%3.0f ", array[i][j]);
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
+
+    }
+
+    Matrix::Matrix(const Matrix& fromMatrix) : m_m{ fromMatrix.m_m }, m_n{ fromMatrix.m_n }
+    {
+        std::cout << "Конструктор глубокого копирования" << std::endl;
+
+        array = new double* [m_m];
+        std::cout << m_m << " " << m_n << std::endl;
+        for (int i = 0; i < m_m; ++i)
+        {
+            array[i] = new double[m_n];
+            for (int j = 0; j < m_n; ++j)
+            {
+                array[i][j] = fromMatrix.array[i][j];
+                printf("%3.0f ", array[i][j]);
+            }
+            std::cout << std::endl;
+        }
+
     }
 }
