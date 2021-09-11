99df7cb5ee456b5cb7a89a37a039d01da4963563
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2021-09-11 23:40:04 +0700

    Update Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -8,73 +8,59 @@ namespace luzalex
     private:
         int m_m, m_n;//m_m - кол-во строк; m_n - кол-во столбцов
         double** array;
+        static int s_idGenerator;
+        int m_id;
 
     public:
-        Matrix()
+        Matrix(): m_id{ s_idGenerator++ }
         {
-            std::cout << "Конструктор по умолчанию" << std::endl;
+            std::cout << "Конструктор по умолчанию #" << m_id << std::endl;
             Matrix(3, 3);
         }
-        Matrix(int m)
+        Matrix(int m): m_id{ s_idGenerator++ }
         {
-            std::cout << "Конструктор - квадратная матрица" << std::endl;
+            std::cout << "Конструктор #" << m_id << " - квадратная матрица" << std::endl;
             Matrix(m, m);
         }
-
         Matrix(int m, int n);
         Matrix(const Matrix& fromMatrix);
         ~Matrix()
         {
-            std::cout << "Деструктор" << std::endl;
+            std::cout << "Деструктор матрицы #" << m_id << std::endl;
             for (int i = 0; i < m_m; ++i)
                 delete[] array[i];
             delete[] array;
         }
 
-        friend bool canMltpl(const Matrix& A, const Matrix& B)
-        {
-            return A.m_n == B.m_m;
-        }
+        friend const bool canMltpl(const Matrix& A, const Matrix& B)
+        { return A.m_n == B.m_m; }
 
-        friend bool canAdd(const Matrix& A, const Matrix& B)
-        {
-            return A.m_m == B.m_m && A.m_n == B.m_n;
-        }
+        friend const bool canAdd(const Matrix& A, const Matrix& B)
+        { return A.m_m == B.m_m && A.m_n == B.m_n; }
 
-        double maxItem()
-        {
-            double max = array[0][0];
-            for (int i = 0; i < m_m; ++i)
-                for (int j = 0; j < m_n; ++j)
-                    if (max < array[i][j])
-                        max = array[i][j];
-            return max;
-        }
-        double minItem()
-        {
-            double min = array[0][0];
-            for (int i = 0; i < m_m; ++i)
-                for (int j = 0; j < m_n; ++j)
-                    if (min > array[i][j])
-                        min = array[i][j];
-            return min;
-        }
+        const double maxItem();
+        const double minItem();
+        
 
         //дружественность функции позволяет напрямую обращаться к закрытому члену класса
-        friend Matrix operator+(const Matrix& A, const Matrix& B);
-        friend Matrix operator-(const Matrix& A, const Matrix& B);
-        friend Matrix operator*(const Matrix& A, const Matrix& B);
-        friend Matrix operator*(const Matrix& A, const int k);
+        //перегрузка бинарных операторов для матриц (сложение, вычитание, умножение матриц), если это возможно (проверка есть)
+        friend const Matrix operator+(const Matrix& A, const Matrix& B);
+        friend const Matrix operator-(const Matrix& A, const Matrix& B);
+        friend const Matrix operator*(const Matrix& A, const Matrix& B);
+        /* Перегрузка оператора* для умножения матрицы на скаляр
+           с сохранением первоначальной матрицы и копированием умноженной матрицы с возвратом в caller*/
+        friend const Matrix operator*(const Matrix& A, const double k);
+        // Перегрузка оператора вывода матрицы
         friend std::ostream& operator<<(std::ostream& out, const Matrix& matrix);
 
 
         // Перегрузка оператора присваивания как метода класса (передаётся неявный объект - левый операнд)
-        Matrix& operator=(const Matrix& matrix);
-        // Перегрузка оператора суммы с присваиванием как метода класса
-        Matrix& operator+=(const Matrix& matrix);
-        Matrix& operator-=(const Matrix& matrix);
-        Matrix& operator*=(const Matrix& matrix);
-        Matrix& operator*=(const int k);
+        const Matrix& operator=(const Matrix& matrix);
+        // Перегрузка оператора суммы/разности/умножения_матриц/умножения_на_скаляр с присваиванием как метода класса
+        const Matrix& operator+=(const Matrix& matrix);
+        const Matrix& operator-=(const Matrix& matrix);
+        const Matrix& operator*=(const Matrix& matrix);
+        const Matrix& operator*=(const double k);
 
         //Оператор доступа к члену
         //double* operator[](const Matrix& matrix);
