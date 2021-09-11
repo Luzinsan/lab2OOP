f94f1f8752a6bd33d22509ced9f79b0b31decdb3
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2021-09-11 23:40:21 +0700

    Update Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -2,9 +2,11 @@
 
 namespace luzalex
 {
-    Matrix::Matrix(int m, int n) : m_m{ (m % 11) }, m_n{ (n % 11) }
+    int Matrix::s_idGenerator = 1;
+
+    Matrix::Matrix(int m, int n) : m_m{ (m % 10) }, m_n{ (n % 10) }, m_id{ s_idGenerator++}
     {
-        std::cout << "Конструктор с параметрами m = " << m << " и n = " << n << std::endl;
+        std::cout << "Конструктор с параметрами m = " << m << " и n = " << n << "  под номером #" << m_id << std::endl;
         array = new double* [m_m];
         std::cout << m_m << " " << m_n << std::endl;
         for (int i = 0; i < m_m; ++i)
@@ -17,12 +19,12 @@ namespace luzalex
             }
             std::cout << std::endl;
         }
-
+        std::cout << std::endl;
     }
 
-    Matrix::Matrix(const Matrix& fromMatrix) : m_m{ fromMatrix.m_m }, m_n{ fromMatrix.m_n }
+    Matrix::Matrix(const Matrix& fromMatrix) : m_m{ fromMatrix.m_m }, m_n{ fromMatrix.m_n },  m_id{ s_idGenerator++ }
     {
-        std::cout << "Конструктор глубокого копирования" << std::endl;
+        std::cout << "Конструктор глубокого копирования #"<< m_id << std::endl;
 
         array = new double* [m_m];
         std::cout << m_m << " " << m_n << std::endl;
@@ -36,6 +38,26 @@ namespace luzalex
             }
             std::cout << std::endl;
         }
+        std::cout << std::endl;
+    }
 
+    const double Matrix::maxItem()
+    {
+        double max = array[0][0];
+        for (int i = 0; i < m_m; ++i)
+            for (int j = 0; j < m_n; ++j)
+                if (max < array[i][j])
+                    max = array[i][j];
+        return max;
+    }
+
+    const double Matrix::minItem()
+    {
+        double min = array[0][0];
+        for (int i = 0; i < m_m; ++i)
+            for (int j = 0; j < m_n; ++j)
+                if (min > array[i][j])
+                    min = array[i][j];
+        return min;
     }
 }
