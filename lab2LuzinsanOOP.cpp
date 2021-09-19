d13ebc61ef6c4be71574c513013a08bbf6266406
luzinsan <53607329+Luzinsan@users.noreply.github.com>
2021-09-19 23:06:24 +0700

    Update lab2LuzinsanOOP.cpp

--- lab2LuzinsanOOP.cpp
+++ lab2LuzinsanOOP.cpp
@@ -1,24 +1,69 @@
-#include <ctime>
 #include "Matrix.h"
 
-using namespace luzalex;
+using namespace luMath;
 
 int main()
 {
+    system("color 1E");
     setlocale(LC_ALL, "Russian");
     srand(static_cast<unsigned int>(time(0)));
     rand();
 
-    std::cout << "asterix: \n" << std::endl;
     Matrix asterix;
-    std::cout << "masterix: \n" << std::endl;
-    Matrix masterix(rand()%10);
-    std::cout << "matrix: \n" << std::endl;
-    Matrix matrix(rand()%10, rand() % 10);
-    std::cout << "maMatrix: \n" << std::endl;
+    std::cout << "asterix: \n" << asterix << std::endl;
+    
+    Matrix masterix(8);
+    std::cout << "masterix: \n" << masterix << std::endl;
+    
+    Matrix matrix(4, 8);
+    std::cout << "matrix: \n" << matrix << std::endl;
+   
     Matrix maMatrix(matrix);
-    std::cout << "abelix: \n" << std::endl;
+    std::cout << "maMatrix: \n" << maMatrix << std::endl;
+    
     Matrix abelix(matrix + maMatrix);
+    std::cout << "abelix: \n" << matrix << '+' << maMatrix << '=' << abelix << std::endl;
+   
+    Matrix matrix1(matrix - maMatrix);
+    std::cout << "matrix1: \n" << matrix << '-' << maMatrix << '=' << matrix1 << std::endl;
+
+    Matrix matrix2(matrix * masterix);
+    std::cout << "matrix2: \n" << matrix << '*' << masterix << "=" << matrix2 << std::endl;
+
+    std::cout << "The max item of matrix2: \n" << matrix2 << "\t\tis " << matrix2.maxItem() << std::endl;
+    std::cout << "The min item of matrix2: \n" << matrix2 << "\t\tis " << matrix2.minItem() << std::endl;
+    int k = 3;
+    Matrix matrix3(matrix * k);
+    std::cout << "matrix3: \n" << matrix << '*' << k << "\n=" << matrix3 << std::endl;
+
+    Matrix matrix4(4, 8);
+    matrix4 = matrix;
+
+    std::cout << "matrix4: \n" << matrix4 << "+=" << abelix << '=' << std::endl;
+    matrix4 += abelix;
+    std::cout << matrix4 << std::endl;
+
+    std::cout << "matrix4: \n" << matrix4 << "-=" << abelix << '=' << std::endl;
+    matrix4 -= abelix;
+    std::cout << matrix4 << std::endl;
+
+    std::cout << "matrix4: \n" << matrix4 << "*=" << masterix << '=' << std::endl;
+    matrix4 *= masterix;
+    std::cout << matrix4 << std::endl;
+
+    std::cout << "matrix4: \n" << matrix4 << "*=" << k << "\n=" << std::endl;
+    matrix4 *= k;
+    std::cout << matrix4 << std::endl;
+    
+    std::cout << "matrix4[3][7]: \n" << matrix4[3][7] << std::endl;
+
+    Matrix matrix5(3);
+    std::cin >> matrix5;
+    Matrix matrix6(3);
+    std::cin >> matrix6;
+    std::cout << "matrix6: \n" << matrix5 << "*=" << matrix6 << '=' << std::endl;
+    matrix5 *= matrix6;
+    std::cout << matrix5 << std::endl;
 
     return 0;
 }
